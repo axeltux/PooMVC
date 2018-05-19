c38a992d428eea6cc4290f5dd117dcdd8694e915
axeltux <axeltux@gmail.com>
2018-05-19 16:49:12 -0500

    first commit

---
+++ usuarios/constants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexis Torres
+ * Date: 19/05/2018
+ * Time: 04:33 PM
+ */
+
+    const MODULO = 'usuarios/';
+    # controladores
+    const SET_USER = 'set';
+    const GET_USER = 'get';
+    const DELETE_USER = 'delete';
+    const EDIT_USER = 'edit';
+    # vistas
+    const VIEW_SET_USER = 'agregar';
+    const VIEW_GET_USER = 'buscar';
+    const VIEW_DELETE_USER = 'borrar';
+    const VIEW_EDIT_USER = 'modificar';
+?>
